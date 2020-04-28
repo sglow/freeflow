c67fb16267070356fb64702d191ad3ba9ab9271a
Steve Glow <sglow@embeddedintelligence.com>
2020-04-28 17:15:42 -0400

    Fixed bad footprint for 20 pin headers. Added gerber files for sensor boards

--- electronics/makepackages.py
+++ electronics/makepackages.py
@@ -111,15 +111,9 @@ p.Print( 'packages/dual_fet' )
 
 # Gage pressure sensor.  Note that pins are numbered backward from normal
 p = Part();
-p.InitFourSided( N=12, W=mm(5), L=mm(5), pitch=mm(1.27), pinW=mm(0.7), pinL=mm(0.65), rowD=mm(4.2), exact=True )
+p.InitFourSided( N=12, W=mm(5), L=mm(5), pitch=mm(1.27), pinW=mm(0.7), pinL=mm(0.65), rowD=mm(4.2), exact=True, mirror=True )
 
-# Reverse pin numbering
-for i in p.pins: i.X *= -1;
-
-# Add outline with the pin 1 dot is in the right place
-p.lines=[]
-p.AddOutline( H=mm(5), W=mm(5), type="BOX", mirror=True )
-
+# Add vent hole
 pi = PinInfo( pinW=mm(1), exact=True )
 p.pins.append( Pin( n=-1, x=mm(2.05-4.2/2), y=mm(4.2/2-1.24), info=pi ) )
 p.Info( pnum='MPRLS0001PG0000SA', mfg='Honeywell', dk='480-7100-1-ND' );
@@ -212,13 +206,13 @@ p.Print( "packages/testpoint" );
 
 # Hirose 20 pin socket connector
 p = Header()
-p.Create( N=20, pinW=mm(.25), pinL=mm(1.1), rows=2, L=mm(7.6), W=mm(5), pitch=mm(.5), padExt=mm(6.6), exact=True, smt=True );
+p.Create( N=20, pinW=mm(.25), pinL=mm(1.1), rows=2, L=mm(7.6), W=mm(5), pitch=mm(.5), padExt=mm(6.6), exact=True, smt=True, bottom=True );
 p.Info( mfg='Hirose', pnum='DF23C-20DS-0.5V(51)', dk='H124690CT-ND' )
 p.Print( 'packages/header2x10s' )
       
 # Hirose 20 pin plug connector
 p = Header()
-p.Create( N=20, pinW=mm(.25), pinL=mm(1.0), rows=2, L=mm(6.5), W=mm(3.1), pitch=mm(.5), padExt=mm(4.9), exact=True, smt=True, bottom=True );
+p.Create( N=20, pinW=mm(.25), pinL=mm(1.0), rows=2, L=mm(6.5), W=mm(3.1), pitch=mm(.5), padExt=mm(4.9), exact=True, smt=True );
 p.Info( mfg='Hirose', pnum='DF23C-20DP-0.5V(92)', dk='H124749CT-ND' )
 p.Print( 'packages/header2x10p' )
       
@@ -238,4 +232,14 @@ p = RectPart()
 p.Create( N=8, W=mm(2), L=mm(2.5), pitch=mm(.65), pinW=mm(.35), pinL=mm(0.5), rowD=mm(1.1+0.5), exact=True, outline="BOX", mirror=True )
 p.Info( pnum='BMP280', mfg='Bosch', dk='828-1064-1-ND' )
 p.Print( 'packages/BMP280' )
+
+# BMP388 - Another pressure sensor option
+p = RectPart()
+p.Create( N=8, W=mm(2), L=mm(2.5), pitch=mm(.65), pinW=mm(.35), pinL=mm(0.5), rowD=mm(1.1+0.5), exact=True, outline="BOX", mirror=True )
+p.Info( pnum='BMP280', mfg='Bosch', dk='828-1064-1-ND' )
+p.Print( 'packages/BMP280' )
+
+p = Part();
+p.InitFourSided( N=[2,3], W=mm(2.5), L=mm(2.5), pitch=mm(0.5), pinW=mm(0.25), pinL=mm(0.275), rowD=mm(1.525), exact=True )
+p.Print( 'packages/BMP388' )
 
