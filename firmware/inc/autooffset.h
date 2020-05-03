44874264c7f9af308390e72d886dff59f53762d0
Steve Glow <sglow@embeddedintelligence.com>
2020-05-03 14:31:04 -0400

    Header for auto-offset code

---
+++ firmware/inc/autooffset.h
@@ -0,0 +1,14 @@
+/* autooffset.h */
+
+#ifndef _DEF_INC_AUTOOFFSET
+#define _DEF_INC_AUTOOFFSET
+
+// prototypes
+void InitAutoOffset( void );
+void LoopUpdtOffset( void );
+float GetPresFilt1( void );
+float GetPresFilt2( void );
+float GetAutoOffset( void );
+void AutoOffsetClear( void );
+
+#endif
